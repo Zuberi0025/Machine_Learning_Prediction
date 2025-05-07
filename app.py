548721b131fa77b25ff9073e94583f8210f43883
unknown <mabalozy22@gmail.com>
2025-05-07 10:42:20 +0300

    update changes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from pages import data_exploration, model_training,prediction_page
 from data_utilis import load_data
 #set configurations
 st.set_page_config(
@@ -12,11 +11,5 @@ st.title("Climate Trend Analyis and Prediction")
 st.markdown(" Analysis historical temperature and predict future trends")
 st.sidebar.title("RESOURCES")
 page=st.sidebar.radio("Go to",['Data Exploration','Model Training','Prediction'])
-#Display selected page
-if page =="Data Exploration":
-    data_exploration.show(df)
-elif page =="Model Training":
-    model_training.show(df)
-else:
-    prediction_page.show(df)
+
     
